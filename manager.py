c677336bd2d18044b0d0d6ac4e8535b6ee625caf
Igor Petruk <igor.petrouk@gmail.com>
2015-01-31 19:51:51 +0000

    Added basic DI and refactored manager.py with DI in mind

--- manager.py
+++ manager.py
@@ -2,39 +2,26 @@ import os
 
 import common
 import config
-import graph
 import logging
-import functools
-import notify
-import build
 import time
 
 class Manager(object):
-    def __init__(self):
-        self.active_targets = set()
-        
-        self.graph = graph.DependencyTracker(functools.partial(Manager.GetDependencies, self))
-        self.graph.AddTrackedHandler(functools.partial(Manager.OnTracked, self))
-        self.graph.AddUntrackedHandler(functools.partial(Manager.OnUntracked, self))
-        self.graph.AddRefreshingHandler(functools.partial(Manager.OnRefreshedAsDependency, self))
-        
-        self.tracker = notify.TargetWatcher()
-        self.tracker.AddModificationHandler(functools.partial(Manager.OnModifiedFiles, self))
-    
-        self.build_tracker = build.BuildTracker(self.graph)
+    def __init__(self, graph, target_watcher, build_tracker, config_evaluator):
+        self.graph = graph
+        self.target_watcher = target_watcher
+        self.build_tracker = build_tracker
+        self.config_evaluator = config_evaluator
 
     def Join(self):
-        self.tracker.Join()
+        self.target_watcher.Join()
 
     def GetDependencies(self, dep):
         target = self._LoadTarget(dep)
         return set(target.GetConfig().deps)
 
     def AddActiveTarget(self, target_name):
-        if target_name not in self.active_targets:
-            self.active_targets.add(target_name)
-            self.graph.AddTopLevelTarget(target_name)
-            self.build_tracker.Build()
+        self.graph.AddTopLevelTarget(target_name)
+        self.build_tracker.Build()
 
     def _AddTarget(self, target_name):
         target = self._LoadTarget(target_name)
@@ -47,11 +34,11 @@ class Manager(object):
     def OnTracked(self, target_name):
         logging.info("Manager tracking %s", target_name)
         self._AddTarget(target_name)
-        self.tracker.AddTarget(self.build_tracker.GetTarget(target_name))
+        self.target_watcher.AddTarget(self.build_tracker.GetTarget(target_name))
 
     def OnUntracked(self, target_name):
         logging.info("Manager untracking %s", target_name)
-        self.tracker.RemoveTarget(self.build_tracker.GetTarget(target_name))
+        self.target_watcher.RemoveTarget(self.build_tracker.GetTarget(target_name))
         self._RemoveTarget(target_name)
 
     def OnRefreshedAsDependency(self, target_name):
@@ -66,16 +53,14 @@ class Manager(object):
         for modified_other_files_item in modified_other_files:
             self.graph.RefreshTarget(modified_other_files_item.GetName())
         started_build = time.time()
-        logging.info("Changes detection took %.0f ms", (started_build - started_eval)*1000)
+        logging.info("Changes detection took %.0f ms",
+                (started_build - started_eval)*1000)
         self.build_tracker.Build()
-        logging.info("Building changed targets took %.3f sec", time.time() - started_build)
+        logging.info("Building changed targets took %.3f sec",
+                time.time() - started_build)
 
     def _LoadTarget(self, target_name):
         target = common.Target(target_name)
-        evaluator = config.Evaluator()
-        evaluator.RefreshConfig(target)
+        self.config_evaluator.RefreshConfig(target)
         return target
 
-m = Manager()
-m.AddActiveTarget("mathapp/main")
-m.Join()
--- ni.py
+++ ni.py
@@ -1,9 +1,46 @@
 #!/usr/bin/python
 
-class Context(object):
+import functools
+
+import config
+import build
+import notify
+import manager
+import graph
+
+class App(object):
 
     def __init__(self):
-        pass
+        
+        self.graph = graph.DependencyTracker(
+                lambda deps: self.manager.GetDependencies(deps))
+        
+        self.build_tracker = build.BuildTracker(self.graph)
+        
+        self.target_watcher = notify.TargetWatcher()
+        
+        self.config_evaluator = config.Evaluator()
 
+        self.manager = manager.Manager(
+                self.graph,
+                self.target_watcher,
+                self.build_tracker,
+                self.config_evaluator)
+        
+        # Post init
+        self.graph.AddTrackedHandler(
+                functools.partial(manager.Manager.OnTracked, self.manager))
+        self.graph.AddUntrackedHandler(
+                functools.partial(manager.Manager.OnUntracked, self.manager))
+        self.graph.AddRefreshingHandler(
+                functools.partial(manager.Manager.OnRefreshedAsDependency, self.manager))
+        
+        self.target_watcher.AddModificationHandler(
+                functools.partial(manager.Manager.OnModifiedFiles, self.manager))
+    
+    def Run(self):
+        self.manager.AddActiveTarget("mathapp/main")
+        self.manager.Join()
 
-Context().Run()
+app = App()
+app.Run()
