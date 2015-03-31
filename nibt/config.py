27b4191dc4ffa8334be2cd717f533afaa3eebbed
Igor Petruk <igor.petrouk@gmail.com>
2015-03-30 21:24:59 -0700

    Added default configuration from the egg and deps in setup.py

--- nibt/config.py
+++ nibt/config.py
@@ -1,17 +1,24 @@
 import configparser
 import os
+import logging
 
 class Configuration(object):
     def __init__(self, config_files_list):
         if config_files_list:
             self.config = configparser.ConfigParser()
-            my_config_path = os.path.expanduser(config_files_list[0])
-            if os.path.exists(my_config_path):
-                self.config.read(os.path.expanduser(my_config_path))
+            current_config_file = config_files_list[0]
+            if isinstance(current_config_file, str):
+                logging.info("Reading config from path %s", current_config_file)
+                my_config_path = os.path.expanduser(current_config_file)
+                if os.path.exists(my_config_path):
+                    self.config.read(os.path.expanduser(my_config_path))
+            else:
+                logging.info("Reading config from stream %s", current_config_file)
+                config_str =  ''.join([x.decode() for x in current_config_file.readlines()])
+                self.config.read_string(config_str)
             self.next_configuration = Configuration(config_files_list[1:])
         else:
             self.config = None
-
         
     def Get(self, section, key, raise_exception=True, default=None):
         if not self.config:
