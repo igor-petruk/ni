50af04d8981934f2be3b8ab442a882d55c92bd69
Igor Petruk <igor.petrouk@gmail.com>
2015-04-12 22:30:37 +0100

    Added proper check during C++ binary symlink creating, otherwise consecutive creating fail

--- nibt/cpp.py
+++ nibt/cpp.py
@@ -138,7 +138,8 @@ class CppBinaryBuilder(CppStaticLibraryBuilder):
         if definition.binary_name:
             symlink_full_path = os.path.join(target.GetRootDir(), "bin", definition.binary_name)
             logging.info("Symlinking %s to %s", binary_name, symlink_full_path)
-            os.makedirs(os.path.dirname(symlink_full_path))
+            if not os.path.exists(os.path.dirname(symlink_full_path):
+                os.makedirs(os.path.dirname(symlink_full_path))
             os.symlink(binary_name, symlink_full_path)
 
         return [CppStaticBinary(binary_name)]
